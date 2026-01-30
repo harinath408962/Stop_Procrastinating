988ef357d38973ff35b69d074bf2e22544f8125f
harinath408962 <harinath22082006@gmail.com>
2026-01-30 19:29:58 +0530

    Update build artifacts for Feedback button

--- docs/assets/firebase-DHWCcEE5.js
+++ docs/assets/firebase-DHWCcEE5.js
@@ -1,4 +1,4 @@
-import{o as Lh}from"./vendor-DgLGewnu.js";const xh=()=>{};var Ia={};/**
+import{o as Lh}from"./vendor-BxH-JlEu.js";const xh=()=>{};var Ia={};/**
  * @license
  * Copyright 2017 Google LLC
  *
